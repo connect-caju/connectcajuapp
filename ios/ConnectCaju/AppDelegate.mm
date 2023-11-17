a7fa26fd4943efe182b30c778d453e6b8256e8c5
musekwa <musekwa2011@gmail.com>
2023-11-17 11:03:25 +0200

    changed the app name to Connect Caju

--- ios/ConnectCaju/AppDelegate.mm
+++ ios/ConnectCaju/AppDelegate.mm
@@ -44,7 +44,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"farmersapp", initProps);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"Connect Caju", initProps);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
--- ios/ConnectCajuTests/ConnectCajuTests.m
+++ ios/ConnectCajuTests/ConnectCajuTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface farmersappTests : XCTestCase
+@interface ConnectCajuTests : XCTestCase
 
 @end
 
-@implementation farmersappTests
+@implementation ConnectCajuTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
